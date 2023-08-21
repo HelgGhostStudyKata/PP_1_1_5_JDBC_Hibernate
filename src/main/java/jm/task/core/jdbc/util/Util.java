bbd9982ccdbfb5db0cae151bd9e2e5d926333f18
HelgGhost <helgghost@yandex.ru>
2023-08-21 17:00:01 +0700

    1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,6 +4,9 @@ import java.sql.Statement;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql";
+    private final static String USER = "";
+    private final static String PASSWORD = "";
     public static Statement getStatement() {
 
         return null;
