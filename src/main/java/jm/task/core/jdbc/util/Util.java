a388f5118b9a5efcc7879b48c0b7a6350e806bbf
HelgGhost <helgghost@yandex.ru>
2023-08-21 16:55:27 +0700

    1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,11 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static Statement getStatement() {
+
+        return null;
+    }
 }
